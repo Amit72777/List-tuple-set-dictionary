7777c4e92c20d1ae6e27ed9f2cc6c434596565ee
Amit Kushwaha <145186713+Amit72777@users.noreply.github.com>
2024-01-27 23:18:41 +0530

    join & split function are use in this repository

---
+++ join_split.py
@@ -0,0 +1,10 @@
+fruits = ["You", "Are", "Good","Boy"]
+#  join mehtod
+a= ' '.join(fruits)
+print(a)
+
+# split method 
+
+
+# this function work as find the space in string and seperate it into a list 
+print(a.split())
